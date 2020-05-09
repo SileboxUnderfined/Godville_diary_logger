74fec5c518c80ad1a9057f9e0e3b7c6a48938335
SileboxUnderfined <valerkafm@gmail.com>
2020-05-10 00:38:31 +0300

    version 0.2
    -Оптимизирован код
    -Добавлена фича изменения времени, через которое начинается цикл

--- main.py
+++ main.py
@@ -2,64 +2,87 @@ import time, json, urllib
 from time import sleep as s
 from urllib import request as r
 from urllib import parse as p
-from os.path import isfile
 
 aboutProg = "Godville Diary Logger. Created by Silent Underfined Box(SileboxUnderfined), 2020"
-version = 0.1
+version = 0.2
+phrases = {
+    "quest":{    
+        "nameOfGod":"Введите имя бога: ",
+        "apiKey":"Введите API ключ: ",
+        "countOfCycles":"Введите кол-во циклов, после которых начинается запись действий в дневник: ",
+        "sleepCount":"Введите кол-во секунд, которые надо ожидать до следующей фразы(не меньше 60!): "
+    },
+    "default":{
+        "inited":"инициализирован ли settings.json: ",
+        "nameOfGod":"Имя бога: ",
+        "apiKey":"API ключ: ",
+        "countOfCycles":"Кол-во циклов, после которых начинается запись действий в дневник: ",
+        "sleepCount":"Кол-во секунд, после которых заканчивается цикл: "
+    }    
+}
 
 def enterMainValues():
-    apiKey = input("Введите API ключ: ")    
-    godName = input("Введите имя бога: ")    
-    countOfCycles = input("Введите кол-во циклов, после которых начинается запись действий в дневник: ")
-    mainValuesDict = {"nameOfGod":godName,"apiKey":apiKey,"countOfCycles":countOfCycles}
-    mainValues = list((godName,apiKey,countOfCycles))
+    mainValuesDict = {"inited":1}
+    mainValues = list()
+    phrasesForQuest = phrases["quest"]
+    for i in phrasesForQuest:
+        try:
+            tmp = input(phrasesForQuest[i])
+            int(tmp)
+        except ValueError:
+            None    
+
+        mainValuesDict.update({i:tmp})
+        mainValues.append(tmp)
+
     settingsFileOpenWrite = open("settings.json","w")
     json.dump(mainValuesDict,settingsFileOpenWrite)
     settingsFileOpenWrite.close()
-    return mainValues
 
 def init():
-    global initCount
-    if isfile("settings.json") == False:
-        print("Файл settings.json не найден! Начинаю создание нового")
+    global fileName, jsonFileUrl, inited
+    try:
+        settingsFileOpenRead = open("settings.json",'r')
+    except FileNotFoundError:
+        print("файл с настройками не найден!")
         enterMainValues()
+        init()
 
-    settingsFileOpenRead = open("settings.json",'r')
     settingsFile = json.load(settingsFileOpenRead)
     settingsFileOpenRead.close()
-    mainValues = None
-    if settingsFile["nameOfGod"] == "" or settingsFile["apiKey"] == "":
-        mainValues = enterMainValues()
+    if settingsFile["inited"] == 0:
+        enterMainValues()
+        init()
 
-    else:
-        godName = settingsFile["nameOfGod"]
-        apiKey = settingsFile["apiKey"]
-        maxCountOfCycles = settingsFile["countOfCycles"]
-        maxCountOfCycles = int(maxCountOfCycles)
-        mainValues = list((godName,apiKey,maxCountOfCycles))
-        print("имя бога: {}".format(mainValues[0]))
-        print("API ключ: {}".format(mainValues[1]))
-        print("Кол-во циклов, после которых начинается запись: {}".format(mainValues[2]))
-        print('Вы хотите изменить персонажа? 1 - да, enter - нет')
-        ch = input(">> ")
-        if ch == "1":
-            mainValues = enterMainValues()    
+    mainValues = dict()
+    phrasesD = phrases["default"]
+    for i in settingsFile:
+        mainValues.update({i:settingsFile[i]})
+        print(phrasesD[i] + str(settingsFile[i]))
 
-    global fileName
+    print('Вы хотите изменить персонажа? 1 - да, enter - нет')
+    ch = input(">> ")
+    if ch == "1":
+        enterMainValues()
+        init()    
+
     fileName = input("Введите название файла, в который надо логгировать(без расширения): ")
     fileName = fileName + ".txt"
-    if isfile(fileName) == False:
-        f = open(fileName, 'w')
+    try:
+        f = open(fileName, 'r')
+        print('Продолжим запись уже в существующий файл {}'.format(fileName))   
+ 
+    except FileNotFoundError:
+        f = open(fileName,"w")
         print('файл {} создан'.format(fileName))
-        f.close()
-    else:
-        print('Продолжим запись уже в существующий файл {}'.format(fileName))    
+        f.close()    
 
-    jsonFileUrl = "http://www.godville.net/gods/api/{gn}/{apik}".format(gn=p.quote(mainValues[0]),apik=p.quote(mainValues[1]))
-    initCount = 1
-    return tuple((jsonFileUrl,fileName,maxCountOfCycles))
+    jsonFileUrl = "http://www.godville.net/gods/api/{gn}/{apik}".format(gn=p.quote(mainValues["nameOfGod"]),apik=p.quote(mainValues["apiKey"]))
+    countOfCycles = int(mainValues["countOfCycles"])
+    sleepCount = int(mainValues["sleepCount"])
+    inited = tuple((countOfCycles,sleepCount))
 
-def receive_json_obj(jsonFileUrl):
+def receive_json_obj():
     gotFile = r.urlopen(jsonFileUrl).read()
     objOfJSON = json.loads(gotFile)
     strOfJSON = objOfJSON["diary_last"]
@@ -83,43 +106,36 @@ def writeInFile(diaryMSGList):
     CustomfileWrite.close()
     print("Файл закрыт")    
 
-
-def main(inited, mainF):
-    countOfCycles = mainF[0]
-    diaryMSGList = mainF[1]
-    if initCount == 0:
-        bInited = init()
-        inited[0] = bInited[0]
-        inited[1] = bInited[1]
-        inited[2] = bInited[2]
-
-    diaryLastMSG = receive_json_obj(inited[0])
-    if diaryLastMSG not in diaryMSGList: 
-        timestr = time.strftime("%X",time.localtime())   
-        diaryLastMSG = timestr + ": " + diaryLastMSG
+def main(mainF):
+    diaryLastMSG = receive_json_obj()
+    timestr = time.strftime("%X",time.localtime())   
+    diaryLastMSG = timestr + ": " + diaryLastMSG    
+    if diaryLastMSG not in mainF[1]: 
         print(diaryLastMSG)
-        diaryMSGList.append(diaryLastMSG)
+        mainF[1].append(diaryLastMSG)
 
-    #print(countOfCycles)
-    #print(inited[2])
-    s(60)
-    countOfCycles += 1    
-    if countOfCycles == inited[2]:
-        writeInFile(diaryMSGList)
-        countOfCycles = 0
-        diaryMSGList.clear()
+    if inited[1] < 60:
+        print("Перепишите так, чтобы было больше 60 секунд!")
+        init()
 
-    mainF[0] = countOfCycles
-    mainF[1] = diaryMSGList
-    main(inited,mainF)
+    s(inited[1])
+    mainF[0] += 1    
+    if mainF[0] == inited[0]:
+        writeInFile(mainF[1])
+        mainF[0] = 0
+        mainF[1].clear()
+
+    main(mainF)
+
+def closeProg():
+    input("Нажмите Enter, чтобы завершить работу: ")
+    quit()
 
 if __name__ == "__main__":
     print(aboutProg)
     print("Версия: {}".format(version))
-    global initCount
-    inited = list(("","",""))
     countOfCycles = 0
-    initCount = 0
     diaryMSGList = list()
-    mainF = [countOfCycles,diaryMSGList]    
-    main(inited,mainF)
+    mainF = [countOfCycles,diaryMSGList]
+    init()        
+    main(mainF)
